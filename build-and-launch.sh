44b990ebba3ecf47c3262f1399c498dab122af3c
safeemmd <37902568+safeemmd@users.noreply.github.com>
2020-08-16 20:01:57 +0530

    Update build-and-launch.sh

--- build-and-launch.sh
+++ build-and-launch.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+set -x
 ARTIFACT=`/usr/local/bin/packer build -machine-readable packer-file.json |awk -F, '$0 ~/artifact,0,id/ {print $6}'`
 AMI_ID=`echo $ARTIFACT | cut -d ':' -f2`
 echo 'variable "AMI_ID" { default = "'${AMI_ID}'" }' > amivar.tf
