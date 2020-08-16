51a477263f117f099d9709c4c68e4ade015ed1d5
safeemmd <safeemmd@gmail.com>
2020-08-16 16:16:32 +0530

    updated code

--- scripts/userdata.sh
+++ scripts/userdata.sh
@@ -1,34 +1,27 @@
 #!/bin/bash
 
-systemctl restart jenkins
-USERNAME=teqforce
-jenkins_admin_password=Teqforce!1
+set -x
 
-cd /var/lib/jenkins/users/
+function wait_for_jenkins()
+{
+  while (( 1 )); do
+      echo "waiting for Jenkins to launch on port [8080] ..."
+      
+      nc -zv 127.0.0.1 8080
+      if (( $? == 0 )); then
+          break
+      fi
 
-OLDDIR=$(ls -d admin*)
-DIRNUM=$(ls -d admin* | awk -F'_' '{print $2}')
-NEWDIR=${USERNAME}_${DIRNUM}
-mv $OLDDIR $NEWDIR
+      sleep 10
+  done
 
-cd /var/lib/jenkins/users/
-# sed "/<idToDirectoryNameMap*/a <entry><string>$USERNAME<\/string><string>$NEWDIR<\/string><\/entry>" users.xml > users-1.xml
-
-sed -i "s#admin#${USERNAME}#g" users.xml
-
-cd /var/lib/jenkins/users/$USERNAME*
-
-sed -i "s#admin#${USERNAME}#g" config.xml
-
-sudo chown -R jenkins:jenkins /var/lib/jenkins/users/
-systemctl restart jenkins
-
-sleep 10
-# Calling the function
-updating_jenkins_master_password
+  echo "Jenkins launched"
+}
 
 function updating_jenkins_master_password ()
 {
+  jenkins_admin_password="admin"
+
   cat > /tmp/jenkinsHash.py <<EOF
 import bcrypt
 import sys
@@ -43,11 +36,11 @@ print "{}".format(encrypted_pwd)
 EOF
 
   chmod +x /tmp/jenkinsHash.py
-
+  
   # Wait till /var/lib/jenkins/users/admin* folder gets created
   sleep 10
 
-  cd /var/lib/jenkins/users/$USERNAME*
+  cd /var/lib/jenkins/users/admin*
   pwd
   while (( 1 )); do
       echo "Waiting for Jenkins to generate admin user's config file ..."
@@ -59,10 +52,10 @@ EOF
       sleep 10
   done
 
-  echo "Config file created"
+  echo "Admin config file created"
 
   admin_password=$(python /tmp/jenkinsHash.py ${jenkins_admin_password} 2>&1)
-
+  
   # Please do not remove alter quote as it keeps the hash syntax intact or else while substitution, $<character> will be replaced by null
   xmlstarlet -q ed --inplace -u "/user/properties/hudson.security.HudsonPrivateSecurityRealm_-Details/passwordHash" -v '#jbcrypt:'"$admin_password" config.xml
 
@@ -70,3 +63,79 @@ EOF
   systemctl restart jenkins
   sleep 10
 }
+
+function install_packages ()
+{
+
+  wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
+  rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+  yum install -y jenkins
+
+  # firewall
+  #firewall-cmd --permanent --new-service=jenkins
+  #firewall-cmd --permanent --service=jenkins --set-short="Jenkins Service Ports"
+  #firewall-cmd --permanent --service=jenkins --set-description="Jenkins Service firewalld port exceptions"
+  #firewall-cmd --permanent --service=jenkins --add-port=8080/tcp
+  #firewall-cmd --permanent --add-service=jenkins
+  #firewall-cmd --zone=public --add-service=http --permanent
+  #firewall-cmd --reload
+  systemctl enable jenkins
+  systemctl restart jenkins
+  sleep 10
+}
+
+function configure_jenkins_server ()
+{
+  wait_for_jenkins
+
+  sleep 10
+
+  # Jenkins cli
+  echo "installing the Jenkins cli ..."
+  # cp /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar /var/lib/jenkins/jenkins-cli.jar
+  wget -O /var/lib/jenkins/jenkins-cli.jar http://127.0.0.1:8080/jnlpJars/jenkins-cli.jar
+
+  # Getting initial password
+  # PASSWORD=$(cat /var/lib/jenkins/secrets/initialAdminPassword)
+  PASSWORD="admin"
+  sleep 10
+
+  jenkins_dir="/var/lib/jenkins"
+  plugins_dir="$jenkins_dir/plugins"
+
+  cd $jenkins_dir
+
+  # Open JNLP port
+  xmlstarlet -q ed --inplace -u "/hudson/slaveAgentPort" -v 33453 config.xml
+
+  cd $plugins_dir || { echo "unable to chdir to [$plugins_dir]"; exit 1; }
+
+  # List of plugins that are needed to be installed 
+  plugin_list="git-client git github-api github-oauth github MSBuild ssh-slaves workflow-aggregator ws-cleanup"
+
+  # remove existing plugins, if any ...
+  rm -rfv $plugin_list
+
+  for plugin in $plugin_list; do
+      echo "installing plugin [$plugin] ..."
+      java -jar $jenkins_dir/jenkins-cli.jar -s http://127.0.0.1:8080/ -auth admin:$PASSWORD install-plugin $plugin
+  done
+
+  # Restart jenkins after installing plugins
+  java -jar $jenkins_dir/jenkins-cli.jar -s http://127.0.0.1:8080 -auth admin:$PASSWORD safe-restart
+}
+
+### script starts here ###
+
+# install_packages
+
+wait_for_jenkins
+
+updating_jenkins_master_password
+
+wait_for_jenkins
+
+configure_jenkins_server
+
+echo "Done"
+exit 0
